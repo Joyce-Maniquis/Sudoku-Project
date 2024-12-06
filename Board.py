f0e41366ff00bd70864001cec53dd72d32ca0c26
Jarock12 <josh.arockiasamy@outlook.com>
2024-12-06 17:29:47 -0500

    Update Board.py
    Fixed Cell storage

--- Board.py
+++ Board.py
@@ -24,19 +24,9 @@ class Board:
             pygame.draw.line(self.screen, "black", (size , 0), (size, 540), w)
             pygame.draw.line(self.screen, "black", (0, size), (540, size), w)
             size += 60
-        n = 1
+        
         for i in range(9):
             for j in range(9):
-                x = i+1
-                y = j+1
-                self.cells[n] = Cell(0,x,y,self.screen)
-                self.cells[n+1] = (x,y)
-                self.cells[n].draw()
-                n += 2
-        n = 1
-        for i in range(9):
-            for j in range(9):
-                x, y = i * 60, j * 60
                 value = self.sudoku_list[i][j]
                 cell = Cell(value, i, j, self.screen)
                 self.cells[(i + 1, j + 1)] = cell
@@ -46,7 +36,7 @@ class Board:
     def select(self, row, col):
         if selected_cell is not None:
             selected_cell.selected = False
-        selected_cell = self.cells.get(((row), (col)))
+        selected_cell = self.cells.get((row, col))
         selected_cell.selected = True
         selected_cell.draw()
 
@@ -60,75 +50,80 @@ class Board:
 
     #clears the selected cells value and sketched value
     def clear(self):
-        for i in range(1,162,2):
-            if self.cells[i].selected is True:
-                self.cells[i].set_cell_value(0)
-                self.cells[i].set_sketch_value(0)
-                self.cells[i].draw()
+        for cell in self.cells.values():
+            if cell.selected:
+                cell.set_cell_value(0)
+                cell.set_sketch_value(0)
+                cell.draw()
 
     #sketches the given value into the selected cell
     def sketch(self, value):
-        for i in range(1,162,2):
-            if self.cells[i].selected is True:
-                self.cells[i].set_sketch_value(value)
-                self.cells[i].draw()
+        for cell in self.cells.values():
+            if cell.selected:
+                cell.set_sketch_value(value)
+                cell.draw()
 
     #sets the cell value to the value provided
-    def place_number(self,value):
-        for i in range(1,162,2):
-            if self.cells[i].selected is True:
-                self.cells[i].set_cell_value(value)
+    def place_number(self, value):
+        for cell in self.cells.values():
+            if cell.selected:
+                cell.set_cell_value(value)
 
     #sets all sketch values to 0
     def reset_to_original(self):
-        for i in range(1, 162, 2):
-            self.cells[i].set_sketch_value(0)
+        for cell in self.cells.values():
+            cell.set_sketch_value(0)
 
     #checks if there are any cells with value 0
     def is_full(self):
-        for i in range(1, 162, 2):
-            if self.cells[i].value == 0:
+        for cell in self.cells.values():
+            if cell.value == 0:
                 return False
             return True
 
     #draws every cell to update them
     def update_board(self):
-        for i in range(1, 162, 2):
-            self.cells[i].draw()
+        for cell in self.cells.values():
+            cell.draw()
 
     #returns the row and column of the first cell with value 0
     def find_empty(self):
-        for i in range(1, 162, 2):
+        for i in self.cells:
             if self.cells[i].value == 0:
-                return self.cells[i+1]
+                return i
 
     #checks to see if the sudoku is correctly filled in
     def check_board(self):
-        row = []
-        for i in range(1,146,18):
-            for j in range(i,17+i, 2):
-                if self.cells[j].value not in row:
-                    row.append(self.cells[j].value)
+        line = set()
+        for y in range(1, 10):
+            for x in range(1, 10):
+                if self.cells[(x, y)].value not in line:
+                    line.add(self.cells[(x, y)].value)
                 else:
                     return False
-            row = []
-        col = []
-        for i in range (1,18,2):
-            for j in range(i, 1 + i+18*8,18):
-                if self.cells[j].value not in col:
-                    col.append(self.cells[j].value)
+            line = set()
+            
+        for x in range(1, 10):
+            for y in range(1, 10):
+                if self.cells[(x, y)].value not in line:
+                    line.add(self.cells[(x, y)].value)
                 else:
                     return False
-            col = []
-        box = []
-        for l in range (1,14, 6):
-            for k in range (l, 1 + l+18*8, 18):
-                for i in range (k,k+37,18):
-                    for j in range(i, i+5,2):
-                        if self.cells[j].value not in box:
-                            box.append(self.cells[j].value)
+            line = set()
+            
+        box = set()
+        
+        for outer_x in range(3):
+            for outer_y in range(3):
+                for dx in range(3):
+                    for dy in range(3):
+                        x = (outer_x * 3 + dx) + 1
+                        y = (outer_y * 3 + dy) + 1
+                        if self.cells[(x, y)].value not in box:
+                            box.add(self.cells[(x, y)].value)
                         else:
                             return False
-                box = []
+                box = set()
+        
         return True
 
